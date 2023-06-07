24d6739348a388743dce776dd521b80788305786
root <root@DESKTOP-33UO282.localdomain>
2023-06-07 19:55:12 +0500

    final commit for webhook server

--- controllers/order.js
+++ controllers/order.js
@@ -1,101 +1,53 @@
-const { sendWebhookMessage } = require('../utils/webhooks');
+const router = require('../routes/order');
+const nodemailer = require("nodemailer");
 
+
+let _options = {
+  host: 'smtp.socketlabs.com',
+  port: 587,
+  secure: false, // true for 465, false for other ports
+  auth: {
+      user: 'server15997',
+      pass: 'Rd26PcGs4q8Q7HpJb'
+  },
+};
 
 const handleWebhook = (req, res) => {
   // Process the webhook payload here
-  const webhookData = req.body;
 
-  // Log the received webhook data
-  console.log('Received webhook:', webhookData);
-  // console.log('Labels------:', JSON.stringify(webhookData.labels));
-  console.log('Labels:', webhookData.labels);
+ let _message = {};
+ // Array of email addresses
+ const toEmails = ['anotheremail@example.com', 'moreemail@example.com'];
+// Loop through each alert in req.body.alerts array
+for (let i = 0; i < req.body.alerts.length; i++) {
+  let alert = req.body.alerts[i];
 
+  _message = {
+    from: 'no-reply@pumasight.com',
+    to: toEmails,
+    subject: alert.labels.alertname,
+    html: '<html>' + alert.annotations.description + JSON.stringify(alert.labels) + '</html>'
+  };
 
+  // Call the main function for each alert
+  main(_message);
+}
+
+async function main(message) {
+  let transporter = nodemailer.createTransport(_options);
+  await transporter.sendMail(message, function (error, info) {
+    if (error) {
+      return console.log(error);
+    }
+    console.log("Message sent: %s", info.response);
+  });
+}
   // Send a response
   res.json({ message: 'Webhook received successfully' });
 };
 
 
-const createOrder = async (req, res, next) => {
-
-  const { user_id, product_id, price, amount, tax, total } = req.body.order;
-
-
-  // you may want to store it in a database, and generate an ID automatically
-  const data = {
-    order_id: 'a1b2c3d4e5f6g7h8i9', // demo ID
-    user_id,
-    product_id,
-    price,
-    amount,
-    tax,
-    total,
-  };
-
-
-  // after creating the order, it's the time for creating an event
-  // you can pass in whatever data you want to send with the event
-    try {
-      const result = await sendWebhookMessage('order.created', data)
-      res.status(201).json({ message: 'Order has been created successfully', result })
-    } catch (error) {
-      console.log(error);
-      res.status(error.status).json({ message: error.message });
-    }
-};
-
-const createEvent = async (req, res, next) => {
-
-  // now you've received an event from a specific service,
-  // and you want to create an event to notify your subscribers
-  const { event_type, provider_message, provider_name, status } = req.body.event;
-
-
-  // you may want to query the order data of `order_id` and send it with the message
-  const orderId = req.params.order_id;
-
-  const order = { // ex: order data from a database
-    order_id: orderId,
-    user_id: 'fc741db0a2968c39d9c2a5cc75b05370',
-    product_id: 'b31d032cfdcf47a399990a71e43c5d2a',
-    price: '20.99',
-    amount: '2',
-    tax: '3.8',
-    total: '45.78',
-  };
-
-  
-  // you can form the data object as you like
-  const data = {
-    event_type,           // ex: 'fulfilled'
-    provider_message,     // ex: 'your order may take two days on the way'
-    provider_name,        // ex: 'FedEx'
-    status,               // ex: 'safe'
-    order,                // the order data
-  };
-
-
-  try {
-    const result = await sendWebhookMessage(`order.${event_type}`, data)
-    res.status(201).json({ message: 'Event has been created successfully', result })
-  } catch (error) {
-    console.log(error);
-    res.status(error.status).json({ message: error.message });
-  }
-};
-
-const getOrders = (req, res, next) => {
-  res.status(200).json({ data: 'Your Orders' });
-};
-
-const getOrder = (req, res, next) => {
-  res.status(200).json({ data: 'Your Order' });
-};
-
 module.exports = {
-  createOrder,
-  createEvent,
-  getOrders,
-  getOrder,
+
   handleWebhook,
 };
